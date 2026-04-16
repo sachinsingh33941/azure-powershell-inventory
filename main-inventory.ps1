f9dd0acdbbe866a1ccf1897434a67e94d96bf79d
Sachin Kumar <sachinsingh33941@gmail.com>
2026-04-16 13:38:23 +0530

    Added gitignore to exclude reports

---
+++ main-inventory.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "1. Check Single System"
+Write-Host "2. Full Local Inventory"
+Write-Host "3. Domain Inventory"
+
+$choice = Read-Host "Select option"
+
+switch ($choice) {
+    1 { .\01-basic-connectivity.ps1 }
+    2 { .\05-advanced-inventory.ps1 }
+    3 { .\06-domain-inventory.ps1 }
+}
